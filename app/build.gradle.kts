288e33079d29c5b4364ea717cf3cbfa21aa06c11
Patrice Müller <mueller.patrice@gmail.com>
2023-04-30 11:26:25 +0200

    PROJECT-SETUP | Added LeakCanary

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,10 @@ dependencies {
     implementation("androidx.core:core-ktx:1.10.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
 
+    // Tooling
+    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.10")
+
+
     // Compose
     implementation("androidx.activity:activity-compose:1.7.1")
     implementation(platform("androidx.compose:compose-bom:2022.10.00"))
