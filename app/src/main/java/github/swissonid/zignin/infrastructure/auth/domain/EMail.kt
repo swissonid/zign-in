beea17487d1922a59dbeca7404043d882b4935f7
Patrice Müller <mueller.patrice@gmail.com>
2023-04-30 14:05:11 +0200

    REGISTRATION | Added EMail class to the project

---
+++ app/src/main/java/github/swissonid/zignin/infrastructure/auth/domain/EMail.kt
@@ -0,0 +1,37 @@
+package github.swissonid.zignin.infrastructure.auth.domain
+
+/**
+ * Email validation regex.
+ * 1. ^[\w-\.]+ : Begins with word characters, underscores or hyphens, and must match one or more
+ * occurrences. Must contain a "@" symbol right after the first characters.
+ * 2. @([\w-]+\.)+ : It matches a word or hyphen, and must contain a dot "." right after the first
+ * character. This match must occur at least one time, and be separated by a dot "."
+ * 3. [\w-]{2,4}$ : The email string must end with two to four word characters
+ */
+private val EMAIL_REGEX = Regex("^[\\w-.]+@([\\w-]+\\.)+\\w{2,4}\$")
+
+@JvmInline
+value class EMail(val value: String) {
+    companion object {
+        fun fromString(value: String): Result<EMail> {
+            return validateEmail(value).map { EMail(it) }
+        }
+    }
+}
+
+
+sealed class NotAValidEMailException : IllegalArgumentException() {
+    object EmptyName : NotAValidEMailException()
+    object OnlyBlankSpace : NotAValidEMailException()
+    object InvalidEMail : NotAValidEMailException()
+}
+
+fun validateEmail(value: String): Result<String> {
+    when {
+        value.isEmpty() -> return Result.failure(NotAValidEMailException.EmptyName)
+        value.isBlank() -> return Result.failure(NotAValidEMailException.OnlyBlankSpace)
+    }
+    EMAIL_REGEX.containsMatchIn(value).let {
+        return if (it) Result.success(value) else Result.failure(NotAValidEMailException.InvalidEMail)
+    }
+}
---
+++ app/src/test/java/github/swissonid/zignin/infrastructure/auth/domain/EMailTest.kt
@@ -0,0 +1,99 @@
+package github.swissonid.zignin.infrastructure.auth.domain
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.runners.Suite
+
+@RunWith(value = Suite::class)
+@Suite.SuiteClasses(
+    InvalidEMailTest::class,
+    ValidEmailTest::class
+)
+class EMailTest
+
+
+@RunWith(value = Parameterized::class)
+class InvalidEMailTest(
+    private val email: String,
+    private val expectedException: NotAValidEMailException
+) {
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(name = "{0}")
+        fun data(): Collection<Array<Any?>> = listOf(
+            arrayOf("", NotAValidEMailException.EmptyName),
+            arrayOf(" ", NotAValidEMailException.OnlyBlankSpace),
+            arrayOf("a", NotAValidEMailException.InvalidEMail),
+            arrayOf("a@b.c", NotAValidEMailException.InvalidEMail),
+            arrayOf("a@.ch", NotAValidEMailException.InvalidEMail),
+            arrayOf("a@.ch", NotAValidEMailException.InvalidEMail),
+            arrayOf("a@@b.ch", NotAValidEMailException.InvalidEMail),
+            arrayOf("a@b@.ch", NotAValidEMailException.InvalidEMail),
+            arrayOf("a@bch", NotAValidEMailException.InvalidEMail),
+        )
+    }
+
+    @Test
+    fun `validateEmail - should throw a NotAValidEMailException`() {
+        val result = validateEmail(email)
+        assertThat(result.isFailure).isTrue()
+        assertThat(result.exceptionOrNull()).isEqualTo(expectedException)
+    }
+
+    @Test
+    fun `EMail_fromString - should throw a NotAValidEMailException`() {
+        val result = EMail.fromString(email)
+        assertThat(result.isFailure).isTrue()
+        assertThat(result.exceptionOrNull()).isEqualTo(expectedException)
+    }
+}
+
+@RunWith(value = Parameterized::class)
+class ValidEmailTest(private val email: String) {
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(name = "{0}")
+        fun data(): Collection<String> = listOf(
+            "a@b.ch",
+            "a@b.nom.co"
+        )
+    }
+
+    @Test
+    fun `EMail_fromString - should return a valid email`() {
+        val result = EMail.fromString(email)
+        assertThat(result.isSuccess).isTrue()
+        assertThat(result.getOrThrow().value).isEqualTo(email)
+    }
+
+    @Test
+    fun `validateEmail - should return a valid email`() {
+        val result = validateEmail(email)
+        assertThat(result.isSuccess).isTrue()
+        assertThat(result.getOrThrow()).isEqualTo(email)
+    }
+}
+
+// This class is intended to be used for debugging purposes only
+/*class EmailTestDebug {
+    @Test
+    fun `should throw a NotAValidEMailException - Debug`() {
+        val result = EMail.fromString("a")
+        assertThat(result.isFailure).isTrue()
+        assertThat(result.exceptionOrNull()).isEqualTo(NotAValidEMailException.InvalidEMail)
+    }
+
+    @Test
+    fun `should be a valid email - Debug`() {
+        val result = EMail.fromString("a@b.ch")
+        assertThat(result.isSuccess).isTrue()
+        assertThat(result.getOrThrow().value).isEqualTo("a@b.ch")
+    }
+}*/
+
+
+
