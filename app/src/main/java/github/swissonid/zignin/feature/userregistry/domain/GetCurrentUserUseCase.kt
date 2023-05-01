aadf302654db15f3ac7cbebba5e10a4584b0734a
Patrice Müller <mueller.patrice@gmail.com>
2023-05-01 23:17:25 +0200

    REGISTRATION | Remove unused code

--- app/src/main/java/github/swissonid/zignin/feature/userregistry/domain/GetCurrentUserUseCase.kt
+++ app/src/main/java/github/swissonid/zignin/feature/userregistry/domain/GetCurrentUserUseCase.kt
@@ -1,12 +1,9 @@
 package github.swissonid.zignin.feature.userregistry.domain
 
-import android.util.Log
 import github.swissonid.zignin.feature.userregistry.domain.model.RegisteredUser
 
 class GetCurrentUserUseCase(private val userRepository: UserRepository) {
     suspend operator fun invoke(): Result<RegisteredUser> {
-        val result = userRepository.currentUser()
-        Log.e("GetCurrentUserUseCase", "Was is failerue${result.isFailure}")
-        return result
+        return userRepository.currentUser()
     }
 }
